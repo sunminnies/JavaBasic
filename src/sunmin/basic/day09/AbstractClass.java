2792dcd5b8fc2e1554b1c53d04a6cc8216735872
sunmin <sunminnies@gmail.com>
2021-03-26 18:21:10 +0900

    추상클래스 메서드 재정의 인터페이스

---
+++ src/sunmin/basic/day09/AbstractClass.java
@@ -0,0 +1,34 @@
+package sunmin.basic.day09;
+
+public class AbstractClass {
+    public static void main(String[] args) {
+        // 추상클래스
+        // 추상메서드를 포함한 클래스
+        // 추상메서드 : 불완전 메서드를 의미
+        // 즉, 메서드 선언부만 존재하고 몸체는 존재하지 않음
+        // 불완전 메서드를 포함하는 불완전 클래스이기 때문에 이 클래스는 객체화가 되지않음
+
+        // 상속관계에 있는 자식클래스가 부모클래스에 정의해 둔 메서드를 사용하지 않고 재정의하는 경우: 오버라이딩
+        // 이처럼 부모클래스의 메서드가 필요없다면 메서드를 완전하게 정의하지 않고 형태만 만들어 놓고
+        // 자식클래스들이 내용을 채우게 하면 어떨까?
+    }
+}
+
+abstract class Animal2 {
+    protected String name;
+    abstract public void cry(); // 추상메서드
+}
+
+class Cat2 extends Animal2 {
+    @Override
+    public void cry() {
+        System.out.println("야옹~ 야옹~");
+    }
+}
+
+class Dog2 extends Animal2 {
+    @Override
+    public void cry() {
+        System.out.println("멍멍멍~");
+    }
+}
